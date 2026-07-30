13abd5ba93541bd8fdcdb3b8a86c9eb8021066e9
Ghost9987 <duchojakub987@gmail.com>
2026-07-30 22:34:25 +0200

    borders

--- src/main.cc
+++ src/main.cc
@@ -1,21 +1,30 @@
-#include "mead.h"
+#include "mead_terminal.h"
 #include "mead_panel.h"
 #include "mead_text.h"
+#include "mead_border.h"
 
 int main() 
 {
     Mead::Terminal& term { Mead::Terminal::GetInstance() };
 
     Mead::Panel root { Mead::Panel::FullScreen() };
+
     Mead::Panel left { Mead::XPercent(50), Mead::YPercent(100), Mead::Location::LEFT };
     Mead::Panel right { Mead::XPercent(50), Mead::YPercent(100), Mead::Location::RIGHT };
 
-    Mead::Text left_text { "Hello, World", Mead::Location::CENTER };
-    Mead::Text right_text { "Hello, World", Mead::Location::CENTER };
+    Mead::Text leftText { "Hello, World", Mead::Location::CENTER };
+    Mead::Text rightText { "Hello, World", Mead::Location::CENTER };
+
+    Mead::Border leftBorder { Mead::Border::Basic() };
+    Mead::Border rightBorder { Mead::Border::Basic() };
 
 
-    left.Add(left_text);
-    right.Add(right_text);
+    left.Add(leftBorder);
+    left.Add(leftText);
+
+    right.Add(rightBorder);
+    right.Add(rightText);
+
     root.Add(left);
     root.Add(right);
 
