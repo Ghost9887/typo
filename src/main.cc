91c45019c5763457dad2318656886609821f8154
Ghost <duchojakub987@gmail.com>
2026-08-01 23:46:43 +0200

    colours

--- src/main.cc
+++ src/main.cc
@@ -2,39 +2,54 @@
 #include "mead_panel.h"
 #include "mead_text.h"
 #include "mead_border.h"
+#include <iostream>
 
 int main() 
 {
-    Mead::Terminal& term { Mead::Terminal::GetInstance() };
+    Mead::Terminal& term = Mead::Terminal::GetInstance();
 
     Mead::Panel root { Mead::Panel::FullScreen() };
-
     Mead::Panel left { Mead::WidthPercent(50), Mead::HeightPercent(100), Mead::Anchor::LEFT };
-
     Mead::Panel right { Mead::WidthPercent(50), Mead::HeightPercent(100), Mead::Anchor::RIGHT };
+    Mead::Panel center { Mead::WidthPercent(50), Mead::HeightPercent(50), Mead::Anchor::CENTER };
 
-    Mead::Text leftText { "Hello, World", Mead::Allignment::CENTER };
-    Mead::Text rightText { "Hello, World", Mead::Allignment::CENTER };
+    Mead::Text panel1 { "Hello, World", Mead::Alignment::TOP_LEFT };
+    Mead::Text panel2 { "Hello, World", Mead::Alignment::TOP_LEFT };
+    Mead::Text panel3 { "Hello, World", Mead::Alignment::TOP_LEFT };
 
-    Mead::Text title { "Title", Mead::Allignment::TITLE_CENTER };
+    Mead::Text title { "Title", Mead::Alignment::TITLE_CENTER };
+    title.SetForegroundColor(Mead::RGB::Blue());
 
-    Mead::Border border { Mead::Border::Round() };
+    Mead::Border border0 { Mead::Border::Round() };
 
-    left.Add(border);
-    left.Add(leftText);
+    Mead::Border border1 { Mead::Border::Round() };
+    border1.SetColor(Mead::RGB::Blue());
+
+    Mead::Border border2 { Mead::Border::Round() };
+    border2.SetColor(Mead::RGB::Red());
+
+    Mead::Border border3 { Mead::Border::Round() };
+    border3.SetColor(Mead::RGB::Green());
+
+    left.Add(border1);
+    left.Add(panel1);
     left.Add(title);
 
-    right.Add(border);
-    right.Add(rightText);
+    right.Add(border2);
+    right.Add(panel2);
+    
+    center.Add(border3);
+    center.Add(panel3);
 
     root.Add(left);
     root.Add(right);
+    root.Add(center);
 
     term.AddPanel(root);
 
     while (true)
     {
-        term.Render(root);
+        term.RenderAll();
         if (term.GetKey() == 'q') break;
     }
 
